c325f1b989210650451e3bce71417c4b93431c93
Devconst-Account <usama@devconst.com>
2025-06-17 23:18:31 +0500

    gh-pages branch created

--- android/app/src/main/kotlin/com/example/assist_landing_page/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.assist_landing_page
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/utils/app_strings.dart
+++
@@ -1,5 +0,0 @@
-const String kAppName = "Assist Landing Page";
-
-//routs================
-
-const String kLandingPageRoute = "/";
